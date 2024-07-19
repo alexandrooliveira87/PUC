1da5c05994256732e82762e009f0c00774653c58
Alexandro Oliveira <113798156+alexandrooliveira87@users.noreply.github.com>
2024-07-18 21:46:09 -0300

    ok

--- 4-) myfinance-web-dotnet-infra/obj/Debug/net8.0/myfinance-web-dotnet-infra.AssemblyInfo.cs
+++ 4-) myfinance-web-dotnet-infra/obj/Debug/net8.0/myfinance-web-dotnet-infra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfinance-web-dotnet-infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531fef6dbe456cd64b3687854e3a67664e571ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51dfeab54debbfa28cd0a57a7cdd514ca590e78")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfinance-web-dotnet-infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfinance-web-dotnet-infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
